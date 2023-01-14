d204dd65e5c70421249571ea40f0bb36e9ef4ed5
Karim Jabbour <kareemjabbour@gmail.com>
2023-01-14 17:20:49 -0500

    Carousel Sponsors

---
+++ js/sponsor_crsl.js
@@ -0,0 +1,25 @@
+const carousel = document.getElementById("sponsor-crsl");
+const prevButton = document.getElementById("prev-button");
+const nextButton = document.getElementById("next-button");
+
+let currentIndex = 0;
+const images = carousel.children;
+const imagesCount = images.length;
+
+prevButton.addEventListener("click", () => {
+  if (currentIndex === 0) {
+    currentIndex = imagesCount - 1;
+  } else {
+    currentIndex--;
+  }
+  carousel.style.marginLeft = `-${currentIndex * 100}%`;
+});
+
+nextButton.addEventListener("click", () => {
+  if (currentIndex === imagesCount - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  carousel.style.marginLeft = `-${currentIndex * 100}%`;
+});
